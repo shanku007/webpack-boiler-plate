3d8199cf1e87b8c95e32d865efdf3374e67d5069
shanku007 <jhashankar0077@gmail.com>
2019-06-19 12:29:58 +0530

    final Webpack

--- src/scripts/Test.js
+++ src/scripts/Test.js
@@ -3,14 +3,13 @@ export default class Test{
 
         this.message = message;
         this.handleClick = this.handleClick.bind(this);
+        this.newmessage = "What the heck is this";
     }
     handleClick(){
         window.alert(this.message);
     }
 
     render(){
-        return `<h1 onclick="${function(){
-            console.log("hey");
-        } }">${this.message}</h1>`;
+        return `<h1 onclick="Test('Hello World').handleClick.bind(this)">${this.message}</h1>`;
     }
 }
